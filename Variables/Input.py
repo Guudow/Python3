2e25a22d4a8fdaa787c03059790af67a00b8163b
guudow <mguudow@aol.com>
2019-07-12 00:34:21 +0200

    input function

--- Variables/Input.py
+++ Variables/Input.py
@@ -2,3 +2,10 @@ _Author_ = 'Mohamed'
 greeting = 'Hello'
 name = input("Please enter your name ")
 print(greeting + " " + name)
+
+name = input("Enter Enployee Name")
+salary = input("Enter Salary")
+company = input("Enter Company name")
+print('printing Employee details')
+print("name ", 'salary', 'Company')
+print(name, salary, company)
